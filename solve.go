dccfaba98e73e2d45d0d587afb9030460439be76
James Pirruccello <james@carbocation.com>
2013-10-19 18:48:36 -0400

    Fleshing out the solver.

--- solve.go
+++ solve.go
@@ -9,10 +9,46 @@ func main() {
     digits := "123456789"
     rows := "ABCDEFGHI"
     cols := digits
-    //squares := sudoku.Cross(rows, cols)
-    unitlist := sudoku.BuildUnitList(rows, cols, []string{"ABC", "DEF", "GHI"}, []string{"123", "456", "789"})
-
     
-
-    fmt.Println(unitlist)
+    squares := sudoku.Cross(rows, cols)
+    fmt.Println("squares: ", squares)
+    unitlist := sudoku.BuildUnitList(rows, cols, []string{"ABC", "DEF", "GHI"}, []string{"123", "456", "789"})
+	fmt.Println("unitlist: ", unitlist)
+	units := map[string][][]string{}
+	for _, square := range squares {
+		Square:
+		for _, v := range unitlist {
+			for _, unitsquare := range v {
+				//If the square is contained within the unitlist, then add this unitlist belongs to this square's unit map 
+				if square == unitsquare {
+					units[square] = append(units[square], v)
+					continue Square
+				}
+			}
+		}
+	}
+	fmt.Println("units: ", units)
+	
+	peers := map[string][]string{}
+	for square, unitlist := range units {
+		for _, unit := range unitlist {
+			NextUnitSquare:
+			for _, unitsquare := range unit {
+				if unitsquare != square {
+					//TODO: if the unitsquare is already in peers[square], don't re-add it.
+					for _, ps := range peers[square] {
+						if ps == unitsquare {
+							continue NextUnitSquare 
+						}
+					}
+					
+					peers[square] = append(peers[square], unitsquare)
+				}
+			}
+		}
+	}
+	fmt.Println("peers[C2]: ", peers["C2"])
+	
+	panic(0)
+	fmt.Println(sudoku.ParseGrid("324|.342|ajfdso28.49....2"))
 }
--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,35 +1,51 @@
+/*
+Package sudoku implements the sudoku solver written about in Peter Norvig's essay,
+"Solving Every Sudoku Puzzle" http://norvig.com/sudoku.html
+
+Go implementation by James Pirruccello, 2013
+*/
 package sudoku
 
 import (
-    //"bytes"
+	//"bytes"
+	"regexp"
+	"strings"
 )
 
+//Take two strings and build a slice of strings, each of which are
+//bigrams taken from every AxB combination
 func Cross(A string, B string) []string {
-    var out []string
+	var out []string
 	for _, i := range A {
-        for _, j := range B {
-            out = append(out, string(i)+string(j))
-        }
-    }
+		for _, j := range B {
+			out = append(out, string(i)+string(j))
+		}
+	}
 
-    return out
+	return out
 }
 
 func BuildUnitList(rows string, cols string, rowBlocks []string, rowCols []string) [][]string {
-    out := [][]string{}
-    for _, c := range cols {
-        out = append(out, Cross(rows, string(c)))
-    }
+	out := [][]string{}
+	for _, c := range cols {
+		out = append(out, Cross(rows, string(c)))
+	}
 
-    for _, r := range rows {
-        out = append(out, Cross(string(r), cols))
-    }
+	for _, r := range rows {
+		out = append(out, Cross(string(r), cols))
+	}
 
-    for _, rs := range rowBlocks {
-        for _, cs := range rowCols {
-            out = append(out, Cross(rs, cs))
-        }
-    }
+	for _, rs := range rowBlocks {
+		for _, cs := range rowCols {
+			out = append(out, Cross(rs, cs))
+		}
+	}
 
-    return out
+	return out
+}
+
+//Convert any representation of a grid into a valid one for our internal representation
+func ParseGrid(Grid string) string {
+	re := regexp.MustCompile(`[0-9]*\.*`)
+	return strings.Join(re.FindAllString(Grid, -1), "")
 }
